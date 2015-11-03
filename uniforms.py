22d2b8a8c32d64c0d19f6deed624d6814acc0160
Stan <sts@odoo.com>
2015-11-03 22:17:14 +0100

    clean up

--- uniforms.py
+++ uniforms.py
@@ -6,27 +6,6 @@ import xmltodict
 from openerp.exceptions import ValidationError
 from datetime import datetime
 
-class ProductCategory(models.Model):
-    _name = 'baseball.product'
-    name = fields.Char(string="Name", required=True)
-    image = fields.Char()
-    price = fields.Float(string="Price")
-    stock = fields.Integer(string="Stock")
-    description = fields.Html()
-
-
-class Cap(models.Model):
-    _name = 'baseball.caps'
-    _inherit = 'baseball.product'
-    caps_size = fields.Many2many('baseball.caps_size', string="Caps")
-
-
-class Jersey(models.Model):
-    _name = 'baseball.jerseys'
-    _inherit = 'baseball.product'
-    jerseys_ids = fields.Many2many('baseball.jerseysitem', string="Size")
-
-
 class JerseyItem(models.Model):
     _name = 'baseball.jerseysitem'
 
@@ -56,16 +35,6 @@ class JerseyItem(models.Model):
         else:
             self.product_id = self.env['product.product']
 
-class JerseySize(models.Model):
-    _name = 'baseball.jerseys_size'
-    name = fields.Char(string="Name")
-    code = fields.Char(string="Code")
-
-
-class CapSize(models.Model):
-    _name = 'baseball.caps_size'
-    code = fields.Char(string="Size")
-    stock = fields.Integer(string="Stock")
 
 class Product(models.Model):
     _inherit = 'product.product'
